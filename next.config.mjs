d5d1b1df1e522a65f2fb7970ddd85ef090380bfd
Juvenal_Silveira <86546205+JuvenalS@users.noreply.github.com>
2024-05-08 23:13:07 +0000

    uploading and managing files

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: {
-          allowedOrigins: ["localhost:3000", "laughing-fortnight-9467qj76gr6c75x4-3000.app.github.dev", "laughing-fortnight-9467qj76gr6c75x4-3100.app.github.dev"],
-        },
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
